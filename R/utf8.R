fae92f92a162db608a0902d27320205f3f6bb18d
Winston Chang <winston@stdout.org>
2021-03-08 20:05:43 -0600

    Add UTF-8 functions

---
+++ R/utf8.R
@@ -0,0 +1,20 @@
+raw_to_utf8 <- function(data) {
+  res <- rawToChar(data)
+  Encoding(res) <- "UTF-8"
+  res
+}
+
+read_raw <- function(file) {
+  readBin(file, "raw", n = file.info(file)$size)
+}
+
+# Read file as UTF-8
+read_utf8 <- function(file) {
+  res <- read_raw(file)
+  raw_to_utf8(res)
+}
+
+# Write text as UTF-8
+write_utf8 <- function(text, ...) {
+  writeBin(charToRaw(enc2utf8(text)), ...)
+}
