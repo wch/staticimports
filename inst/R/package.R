87b03bb885841c86142924988c96438562484316
Winston Chang <winston@stdout.org>
2021-03-10 12:26:04 -0600

    Add version check to is_installed()

--- inst/R/package.R
+++ inst/R/package.R
@@ -2,6 +2,10 @@ is_loaded <- function(x) {
   !is.null(.getNamespace(x))
 }
 
-is_installed <- function(x) {
-  nzchar(system.file(package = x))
+is_installed <- function(package, version = NULL) {
+  installed <- nzchar(system.file(package = package))
+  if (is.null(version)) {
+    return(installed)
+  }
+  installed && isTRUE(utils::packageVersion(package) >= version)
 }
