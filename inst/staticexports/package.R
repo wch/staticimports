976bdd2624dc34e89ec3828a383df399650c83fa
Carson Sievert <cpsievert1@gmail.com>
2021-11-08 13:07:25 -0600

    Update inst/staticexports/package.R
    Co-authored-by: Winston Chang <winston@stdout.org>

--- inst/staticexports/package.R
+++ inst/staticexports/package.R
@@ -10,8 +10,9 @@ is_installed <- function(pkg, version = NULL) {
   installed && isTRUE(get_package_version(pkg) >= version)
 }
 
-# Since I/O can be expensive, only utils::packageVersion() if the package isn't already loaded
 get_package_version <- function(pkg) {
+  # `utils::packageVersion()` can be slow, so first try the fast path of
+  # checking if the package is already loaded.
   ns <- .getNamespace(pkg)
   if (is.null(ns)) {
     utils::packageVersion(pkg)
