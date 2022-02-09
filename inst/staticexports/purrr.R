a127a88fbbb52e886f41109934010675739e09a9
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-02-09 10:56:52 +0100

    same change for discard() as for keep()

--- inst/staticexports/purrr.R
+++ inst/staticexports/purrr.R
@@ -127,12 +127,7 @@ keep <- function(.x, .f, ...) {
 }
 
 discard <- function(.x, .f, ...) {
-  if (is.logical(.f)) {
-    stopifnot(length(.f) == length(.x))
-    .x[!.f]
-  } else {
-    .x[!map_lgl(.x, .f, ...)]
-  }
+  .x[!vapply(.x, .f, ..., FUN.VALUE = NA)]
 }
 
 compact <- function(.x) {
