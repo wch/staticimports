8572009304dbeafc587fe3154942e05d7f2cd7c5
Garrick Aden-Buie <garrick@adenbuie.com>
2022-02-16 11:12:52 -0500

    Add two assertions: `is_shiny_tag()` and `is_AsIs()`
    Adds two assertions I have in a few `utils.R` files.

--- inst/staticexports/assertions.R
+++ inst/staticexports/assertions.R
@@ -2,3 +2,11 @@
 is_string <- function(x) {
   is.character(x) && length(x) == 1 && !is.na(x)
 }
+
+is_shiny_tag <- function(x) {
+  inherits(x, c("shiny.tag", "shiny.tag.list"))
+}
+
+is_AsIs <- function(x) {
+  inherits(x, "AsIs")
+}
