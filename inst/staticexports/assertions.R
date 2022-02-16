1cc1a4939e4161045ce220575cbf0b40dbe4ff0d
Garrick Aden-Buie <garrick@adenbuie.com>
2022-02-16 13:54:27 -0500

    Remove is_shiny_tag()

--- inst/staticexports/assertions.R
+++ inst/staticexports/assertions.R
@@ -3,10 +3,6 @@ is_string <- function(x) {
   is.character(x) && length(x) == 1 && !is.na(x)
 }
 
-is_shiny_tag <- function(x) {
-  inherits(x, c("shiny.tag", "shiny.tag.list"))
-}
-
 is_AsIs <- function(x) {
   inherits(x, "AsIs")
 }
