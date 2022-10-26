a04706df31042aa4b9afc1334784f44dcb2a25a0
Alex Rossell Hayes <alexander@rossellhayes.com>
2022-10-26 15:30:36 -0700

    Add tests for `process_source_texts()`

---
+++ tests/testthat/test-process_source.R
@@ -0,0 +1,64 @@
+test_that("staticexports source parsing", {
+  outdir <- tempfile("staticimports-test")
+  dir.create(outdir)
+  on.exit(unlink(outdir, recursive = TRUE))
+
+  writeLines(
+"# Comment attached to f
+f <- function() 123
+
+# Comment separated from g
+
+g <- local(
+  function() {
+    x <- 123
+    x
+  }
+)
+
+# Comment separated from x
+
+# Comment attached to x
+x <- list(
+  # Comment within the definition
+  1,
+  2,
+  3
+)
+", file.path(outdir, "file1.R"))
+
+  writeLines(
+    "`%infix%` <- function(lhs, rhs) lhs",
+    file.path(outdir, "file2.R")
+  )
+
+  res <- process_source_texts(lapply(dir(outdir, full.names = TRUE), readLines))
+
+  expect_identical(
+    res,
+    list(
+      f = c(
+        "# Comment attached to f",
+        "f <- function() 123"
+      ),
+      g = c(
+        "g <- local(",
+        "  function() {",
+        "    x <- 123",
+        "    x",
+        "  }",
+        ")"
+      ),
+      x = c(
+        "# Comment attached to x",
+        "x <- list(",
+        "  # Comment within the definition",
+        "  1,",
+        "  2,",
+        "  3",
+        ")"
+      ),
+      `%infix%` = "`%infix%` <- function(lhs, rhs) lhs"
+    )
+  )
+})
