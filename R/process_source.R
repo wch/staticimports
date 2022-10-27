47370ae48ac2d72d079fbff2c2111a741a4a5c78
Alex Rossell Hayes <alexander@rossellhayes.com>
2022-10-27 15:01:40 -0700

    Refactor to avoid creating data frames

--- R/process_source.R
+++ R/process_source.R
@@ -15,50 +15,56 @@ process_source_text_one <- function(text) {
   assignment_ops <- parse_data[
     parse_data$token %in% c("LEFT_ASSIGN", "EQ_ASSIGN"),
   ]
-  # Assignments are the parent expressions of assignment operators
-  assignments <- parse_data[parse_data$id %in% assignment_ops$parent, ]
-  # Top-level expressions have a parent attribute of 0
-  object_definitions <- assignments[assignments$parent == 0, ]
+
+  object_definitions_idx <- which(
+    # Object definitions are the parent expressions of assignment operators
+    parse_data$id %in% assignment_ops$parent &
+      # Top-level expressions have a parent attribute of 0
+      parse_data$parent == 0,
+  )
+
+  object_definition_ids <- parse_data[object_definitions_idx, "id"]
 
   result <- list()
 
-  for (i in seq_len(nrow(object_definitions))) {
-    object_definition <- object_definitions[i, ]
+  for (i in seq_along(object_definition_ids)) {
+    id <- object_definition_ids[[i]]
 
-    names(result)[i] <- extract_object_name(
-      parse_data, object_definition, assignment_ops
-    )
+    start_line <- parse_data[parse_data$id == id, "line1"]
+    end_line <- parse_data[parse_data$id == id, "line2"]
 
-    # A leading comment's parent attribute is 0 - the next expression's id
-    leading_comments <- parse_data[parse_data$parent == -object_definition$id, ]
-
-    # Exclude leading comments if there are any empty lines between the comment
+    # A comment's parent attribute is 0 - the next expression's id
+    comments <- parse_data[parse_data$parent == -id, ]
+    # Include comments if there are no empty lines between the comment
     # and the object definition
-    leading_comments <- leading_comments[
-      object_definition$line1 - leading_comments$line1 <=
-        rev(seq_len(nrow(leading_comments))),
-    ]
-
-    staticexport_lines_idx <- seq(
-      min(leading_comments$line1, object_definition$line1),
-      max(leading_comments$line2, object_definition$line2)
+    leading_comments_idx <- which(
+      start_line - comments$line1 <= rev(seq_len(nrow(comments)))
     )
+    comment_lines <- comments[leading_comments_idx, "line1"]
+
+    staticexport_lines_idx <- seq(min(comment_lines, start_line), end_line)
     result[[i]] <- text[staticexport_lines_idx]
+
+    names(result)[[i]] <- extract_object_name(parse_data, id, assignment_ops)
   }
 
   result
 }
 
-extract_object_name <- function(parse_data, object_definition, assignment_ops) {
-  assignment <- assignment_ops[assignment_ops$parent == object_definition$id, ]
+extract_object_name <- function(parse_data, definition_id, assignment_ops) {
+  assignment_idx <- which(assignment_ops$parent == definition_id)
+  assignment_line <- assignment_ops[assignment_idx, "line1"]
+  assignment_col <- assignment_ops[assignment_idx, "col1"]
 
-  object_name_expr <- parse_data[
-    parse_data$parent == object_definition$id &
-      parse_data$line2 <= assignment$line1 &
-      parse_data$col2 < assignment$col1,
-  ]
+  object_name_idx <- which(
+    parse_data$parent == definition_id &
+      parse_data$line2 <= assignment_line &
+      parse_data$col2 < assignment_col
+  )
 
-  result <- utils::getParseText(parse_data, object_name_expr$id)
+  object_name_id <- parse_data[object_name_idx, "id"]
+
+  result <- utils::getParseText(parse_data, object_name_id)
 
   # Remove backticks, so that strings like "`%||%`" are converted to "%||%"
   if (grepl("^`\\S+`$", result)) {
