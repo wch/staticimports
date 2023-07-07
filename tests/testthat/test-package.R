4441f2aa9813045e5fa5c269cfb3c361fc5879f8
Barret Schloerke <barret@posit.co>
2023-07-07 11:03:19 -0400

    Update test to check for warning. Add tests for `numeric_version` and `package_version`

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -2,12 +2,20 @@
 test_that("is_installed(version=) is a character", {
   load_exports()
 
-  expect_error(
+  expect_warning(
     is_installed("staticimports", 1),
     "`version` must be a character string"
   )
-  expect_error(
+  expect_warning(
     is_installed("staticimports", "0.0.0.1"),
+    NA
+  )
+  expect_warning(
+    is_installed("staticimports", package_version("1.2.3")),
+    NA
+  )
+  expect_warning(
+    is_installed("staticimports", numeric_version("1.2.3")),
     NA
   )
 })
